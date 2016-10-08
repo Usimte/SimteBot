5327810bc1738516ee6cc9c44e2e9052e90dab02
alejandro <oacastillol@unal.edu.co>
2016-10-07 22:19:18 -0500

    Algunas implementaciones

--- SimteBot.py
+++ SimteBot.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import telepot
-class Tarea:
+class Tarea:       
         """Las tareas que va a realizar el grupo"""
         group=[]
         def __init__(self,title,shortAbout,about,coordinator,p):
@@ -13,7 +13,19 @@ class Tarea:
                         raise ValueError("Invalid length shortAbout", shortAbout)
                 self.about=about
                 self.coordinator=coordinator
+                self.group.append(coordinator)
                 self.p=float(p)
+        def edit(self,user,about,p):
+                if user in self.group:
+                        self.about=about
+                        self.p=float(p)
+        def add(self,user):
+                if not user in self.group:
+                        self.group.append(user)
+        def show(self):
+                return "Titulo:"+self.title+"\nDescripción corta:"+self.shortAbout+"\nDescripción: "+self.about+"\nCoordinador: "+self.coordinator+"\nGrupo: "+str(self.group)+"\nAvance "+str(self.p)+"%"
+        def showShort(self):
+                return self.title+"\t"+str(self.p)+"% \n"+self.shortAbout+"\n"+self.coordinator
                 
 
 if len(sys.argv)<2:
